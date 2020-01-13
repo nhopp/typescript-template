664af9a44a6abb03a960b2c350a83745dcd9f641
nhopp <nhopper@fuze.com>
2020-01-13 14:03:25 -0800

    Add debugging from npm and vs code

---
+++ test/main.test.ts
@@ -0,0 +1,10 @@
+import * as chai from 'chai';
+
+const expect = chai.expect;
+
+describe('main', () => {
+  it('math still works', () => {
+    const sum = 2 + 2;
+    expect(sum).to.be.eq(4);
+  });
+});
