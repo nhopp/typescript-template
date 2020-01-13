b33901232c3307e3fa65b522c36a1f539d6c1f54
nhopp <nhopper@fuze.com>
2020-01-13 14:09:13 -0800

    Add express

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,9 @@
+import * as express from 'express';
+
 console.log('hi i am typescript!');
+
+const app = express();
+const port = 8080;
+app.listen(port, () => {
+  console.log(`App listening on the port ${port}`);
+});
